9f3e5f7aa280aa7a4f82c2b350a7cdf0fc8b3ff7
Ahmed <ahmed.zammel.info@gmail.com>
2021-03-19 17:55:41 +0100

    2B - dépendances

--- color-game-app/src/app/app-routing.module.ts
+++ color-game-app/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'user/',
+    loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+  },
+  {
+    path: 'game/',
+    loadChildren: () => import('./game/game.module').then(m => m.GameModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
