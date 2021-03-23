7b5aafd71ad68eef27d6c16d22c6e43536dd8ef8
Ahmed <ahmed.zammel.info@gmail.com>
2021-03-23 10:35:36 +0100

    5A - configuration.

--- color-game-app/src/app/game/new/new.component.ts
+++ color-game-app/src/app/game/new/new.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewComponent implements OnInit {
 
-  constructor() { }
+  gridForm: any;
+  table: string[][];
+  constructor(private formBuilder: FormBuilder, private cd: ChangeDetectorRef) { 
+
+  }
 
   ngOnInit() {
+    this.gridForm = this.formBuilder.group({
+      rowField: [3, [Validators.required, Validators.minLength(1)]],
+      colField: [6, [Validators.required, Validators.minLength(1)]]
+    });
+  }
+
+  drawGrid = () => {
+    let rows: number = this.gridForm.get('rowField').value;
+    let cols: number = this.gridForm.get('colField').value;
+
+    this.table = null; //Vider la variable table.
+    //Recréer le table.
+    this.table = new Array(cols);
+    for(let i=0; i < cols; i++) {
+      this.table[i] = new Array<string>(rows);
+    }
+    // Appeler la detection de changement.
+    this.cd.detectChanges();
   }
 
 }
